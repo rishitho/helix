ea03c92ec721181d75cdba7fe152616f890ae545
rishitho <rishitho@users.noreply.github.com>
2023-08-10 12:28:01 +0530

    accordion css and js update

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,14 +1,5 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+$(document).ready(function(){
+  $('.accordion h3').on('click', function(){
+      $(this).siblings('p').toggle();
+  })
+})
